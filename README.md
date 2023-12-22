4608cfcce0daf6c3ad838fabe31f496a297f5e51
indraniel <indraniel@gmail.com>
2023-12-22 11:12:27 -0600

    + initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# cliopatra
+
+This is a fork of the [original cliopatra on cljars][0].
+
+It is based on a more updated version of [tools.cli][1] (version 1.0.219 to be specific).
+
+Otherwise, it's more or less the same as the original.  
+
+I could not find the original source code on GitHub.
+
+## LICENSE
+
+I could not asertain what the original license to [cliopatra][0] was.  Currently, for this fork, I've used the [ISC License][2].
+
+[0]: https://clojars.org/narkisr/cliopatra
+[1]: https://github.com/clojure/tools.cli
+[2]: https://choosealicense.com/licenses/isc/
---
+++ src/cliopatra/impl.clj
@@ -0,0 +1,54 @@
+(ns cliopatra.impl
+  (:require [clojure.string :as str]))
+
+
+(def usage-exception-cause (Throwable. "usage-exception"))
+
+(defn throw-usage-exception [msg]
+  (throw (RuntimeException. msg usage-exception-cause)))
+
+(defn format-missing [missing]
+  (format "missing required arguments: %s\n"
+          (str/join ", " (map name missing))))
+
+(defn required-option? [option]
+  (-> (drop-while (partial not= :required) option) next first))
+
+(defn opt-spec->opt-name-str [op]
+  (-> (take-while #(= \- (first %)) op)
+      last
+      (str/replace #"^-*" "")))
+
+(defn required-options [options]
+  (keep (fn [op] (when (required-option? op)
+                  (-> (opt-spec->opt-name-str op)
+                      keyword)))
+        options))
+
+(defn opts-spec-keys [opts-specs]
+  (for [op opts-specs]
+    (->> (opt-spec->opt-name-str op)
+         (#(str/replace % #"^\[no\-\]" ""))
+         symbol)))
+
+(def ^:dynamic *env* nil)
+
+(defmacro with-environment [env & body]
+  `(binding [*env* ~env]
+     ~@body))
+
+(defn get-environment [& ks]
+  (get-in *env* ks))
+
+(defn get-commands [namespaces]
+  (for [ns' namespaces
+        [sym var*] (ns-interns ns')
+        :when (:cliopatra.command/command (meta var*))]
+    var*))
+
+(defn get-command [namespaces cmd]
+  (first (for [ns' namespaces
+               [sym var*] (ns-interns ns')
+               :when (:cliopatra.command/command (meta var*))
+               :when (= (str sym) cmd)]
+           var*)))
